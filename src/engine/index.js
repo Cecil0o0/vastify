9f0ee34187120b4a47137109aa9129e4928fed66
chenhaojie <jiegithub@gmail.com>
2018-05-30 14:50:09 +0800

    feat: 部署工具相关提出到新的实例对象

--- src/engine/index.js
+++ src/engine/index.js
@@ -10,11 +10,10 @@ const Web = require('./Web')
 const DeployTool = require('./DeployTool')
 const Logger = require('./Logger')
 const { ObjectDeepSet } = require('../helper/utils')
-const DB = require('./DB')
 
 let instance = null
 
-function getIntance (externalConfig = {}) {
+function getIntance(externalConfig = {}) {
   if (!instance) {
     instance = {
       config
@@ -26,18 +25,20 @@ function getIntance (externalConfig = {}) {
     instance.web = new Web()
     // 提供http服务扩展
     instance.web.externalUseREST(instance.seneca)
-    // pm2自动化部署相关工具
-    instance.DeployTool = new DeployTool()
     // 日志组件
     instance.logger = Logger.getInstance(instance.config.logger.winston)
     // 持久化存储组件
-    instance.db = new DB(instance.config.db)
+    instance.db = require('./DB').getInstance(instance.config.db)
   }
   return instance
 }
 
-getIntance()
+function getDeployTool() {
+  // pm2自动化部署相关工具
+  return new DeployTool()
+}
 
 module.exports = {
-  getIntance
+  getIntance,
+  getDeployTool
 }
